2c89810a3102bb7ed3c8a11b6edb8df7a16d739e
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-22 23:33:08 +0200

    Create index.js

---
+++ config/index.js
@@ -0,0 +1,7 @@
+export const PRIMARY = "#f4005f";
+export const SECUNDARY = "#FFF";
+export const SPACING = 10;
+export const fundo = require('../assets/img/fundo.webp');
+export const banner = require('../assets/banner.jpg');
+
+export const user = { name: "ARNALDO", conta: 397153505, saldo: 168 }
