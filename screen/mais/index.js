fbb6d689972836c5ff2d007622b4d794abc4be6e
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-22 23:33:32 +0200

    Create index.js

---
+++ screen/mais/index.js
@@ -0,0 +1,15 @@
+import { Dimensions } from "react-native"
+import { SafeAreaView } from "react-native"
+import { Text, View } from "react-native"
+import { PRIMARY } from "../../config/index";
+import { StatusBar } from "react-native";
+const { width, height } = Dimensions.get('screen');
+export const Mais = () => {
+    return (
+        <SafeAreaView>
+            <StatusBar style="light" backgroundColor={PRIMARY} />
+            <View style={{ width: width, height: height, backgroundColor: PRIMARY }}>
+            </View>
+        </SafeAreaView>
+    )
+}
