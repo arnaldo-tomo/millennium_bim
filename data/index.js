1ac9f7a0f47a5202a981725c63c4ed78b08cee55
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-22 23:33:12 +0200

    Create index.js

---
+++ data/index.js
@@ -0,0 +1,32 @@
+export const categorias =
+    [
+        {
+            id: 1,
+            img: require('../assets/img/icon/edmm.png'),
+            mame: "Credelec"
+        }, {
+            id: 2,
+            img: require('../assets/img/icon/recarga.png'),
+            mame: "Recargas Celular"
+        }, {
+            id: 3,
+            img: require('../assets/img/icon/tv.png'),
+            mame: "Pacotes TV"
+        }, {
+            id: 4,
+            img: require('../assets/img/icon/partilhar.png'),
+            mame: "Transferir"
+        }, {
+            id: 5,
+            img: require('../assets/img/icon/mobile.png'),
+            mame: "Pagar"
+        }, {
+            id: 6,
+            img: require('../assets/img/icon/qr.png'),
+            mame: "Para Pagar"
+        }, {
+            id: 7,
+            img: require('../assets/img/icon/familia.png'),
+            mame: "Pagamentos INSS"
+        }
+    ]
