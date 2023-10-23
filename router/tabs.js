066303cf93747d7aa8fb36065045c299cf8ec88a
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-24 00:36:42 +0200

    Update tabs.js

--- router/tabs.js
+++ router/tabs.js
@@ -1,36 +1,17 @@
 import * as React from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Dia } from '../screen/dia-a-dia';
 import { Transferir } from '../screen/transferir';
 import { Pagar } from '../screen/pagar';
 import { Mais } from '../screen/mais';
 import { PRIMARY, SPACING } from '../config';
+import { StackNavigation } from './stack';
 
-function HomeScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home!</Text>
-        </View>
-    );
-}
-
-function SettingsScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Settings!</Text>
-        </View>
-    );
-}
 
 const Tab = createBottomTabNavigator();
 
 export const TabsNavigation = () => {
     return (
-        // <NavigationContainer>
-
         <Tab.Navigator
             screenOptions={({ route }) => ({
                 tabBarLabelPosition: "below-icon",
@@ -59,7 +40,6 @@ export const TabsNavigation = () => {
                             ? 'ellipsis-horizontal'
                             : 'ellipsis-horizontal-outline';
                     }
-                    // You can return any component that you like here!
                     return <Ionicons name={iconName} size={size} color={color} />;
                 },
                 tabBarActiveTintColor: PRIMARY,
@@ -71,12 +51,11 @@ export const TabsNavigation = () => {
                 }
             }}
         >
-            <Tab.Screen name="Dia-a-dia" component={Dia} options={{ headerShown: false }} />
+            <Tab.Screen name="Dia-a-dia" component={StackNavigation} options={{ headerShown: false }} />
             <Tab.Screen name="Transferir" component={Transferir} options={{ headerShown: false }} />
             <Tab.Screen name="Pagar" component={Pagar} options={{ headerShown: false }} />
             <Tab.Screen name="Mais" component={Mais} options={{ headerShown: false }} />
 
         </Tab.Navigator>
-        // </NavigationContainer>
     );
 }
