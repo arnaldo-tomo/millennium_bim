af0d78b881f51d1f63f07c639eb2986248b02542
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-23 09:23:30 +0200

    Update tabs.js

--- router/tabs.js
+++ router/tabs.js
@@ -29,48 +29,54 @@ const Tab = createBottomTabNavigator();
 
 export const TabsNavigation = () => {
     return (
-        <NavigationContainer>
+        // <NavigationContainer>
 
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarLabelPosition: "below-icon",
-                    tabBarLabelStyle: { fontWeight: 'bold', fontSize: SPACING + 2, marginBottom: SPACING },
-                    tabBarStyle: { height: 60, elevation: 1 },
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarLabelPosition: "below-icon",
+                tabBarLabelStyle: { fontWeight: 'bold', fontSize: SPACING + 2, marginBottom: SPACING },
+                tabBarStyle: { height: 60, elevation: 1 },
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
 
-                        if (route.name === 'Dia-a-dia') {
-                            iconName = focused
-                                ? 'wallet'
-                                : 'wallet-outline';
-                        }
-                        if (route.name === 'Transferir') {
-                            iconName = focused
-                                ? 'arrow-redo'
-                                : 'arrow-redo-outline';
-                        }
-                        if (route.name === 'Pagar') {
-                            iconName = focused
-                                ? 'server'
-                                : 'server-outline';
-                        }
-                        if (route.name === 'Mais') {
-                            iconName = focused
-                                ? 'ellipsis-horizontal'
-                                : 'ellipsis-horizontal-outline';
-                        }
-                        // You can return any component that you like here!
-                        return <Ionicons name={iconName} size={size} color={color} />;
-                    },
-                    tabBarActiveTintColor: PRIMARY,
-                    tabBarInactiveTintColor: 'black',
-                })}
-            >
-                <Tab.Screen name="Dia-a-dia" component={Dia} options={{ headerShown: false }} />
-                <Tab.Screen name="Transferir" component={Transferir} options={{ headerShown: false }} />
-                <Tab.Screen name="Pagar" component={Pagar} options={{ headerShown: false }} />
-                <Tab.Screen name="Mais" component={Mais} options={{ headerShown: false }} />
-            </Tab.Navigator>
-        </NavigationContainer>
+                    if (route.name === 'Dia-a-dia') {
+                        iconName = focused
+                            ? 'wallet'
+                            : 'wallet-outline';
+                    }
+                    if (route.name === 'Transferir') {
+                        iconName = focused
+                            ? 'arrow-redo'
+                            : 'arrow-redo-outline';
+                    }
+                    if (route.name === 'Pagar') {
+                        iconName = focused
+                            ? 'server'
+                            : 'server-outline';
+                    }
+                    if (route.name === 'Mais') {
+                        iconName = focused
+                            ? 'ellipsis-horizontal'
+                            : 'ellipsis-horizontal-outline';
+                    }
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: PRIMARY,
+                tabBarInactiveTintColor: 'black',
+            })}
+            onNavigate={(route) => {
+                if (route.name === "Credelec") {
+                    route.options.popToTop = true;
+                }
+            }}
+        >
+            <Tab.Screen name="Dia-a-dia" component={Dia} options={{ headerShown: false }} />
+            <Tab.Screen name="Transferir" component={Transferir} options={{ headerShown: false }} />
+            <Tab.Screen name="Pagar" component={Pagar} options={{ headerShown: false }} />
+            <Tab.Screen name="Mais" component={Mais} options={{ headerShown: false }} />
+
+        </Tab.Navigator>
+        // </NavigationContainer>
     );
 }
