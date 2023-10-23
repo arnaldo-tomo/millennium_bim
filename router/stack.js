0da9e97bf45a92be7f560bd0ab89b389b0f9d1f9
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-23 09:23:21 +0200

    Update stack.js

--- router/stack.js
+++ router/stack.js
@@ -4,6 +4,8 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Oneboard } from '../screen/oneboarding';
+import { Credelec } from '../screen/Credeleck';
+import { TabsNavigation } from './tabs';
 
 
 const Stack = createNativeStackNavigator();
@@ -12,7 +14,9 @@ export const StackNavigation = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Oneboard" component={Oneboard} options={{ headerShown: false }} />
+                <Stack.Screen name="TabsNavigation" component={TabsNavigation} options={{ headerShown: false }} />
+                <Stack.Screen name="Credelec" component={Credelec} options={{ headerShown: false }} />
+                <Stack.Screen name="Oneboard" component={Oneboard} screenOptions={{ presentation: 'modal' }} />
             </Stack.Navigator>
         </NavigationContainer>
     );
