5ce84a3517d3a22a2a0e3b96b764d3837bf034b5
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-24 00:36:46 +0200

    Update index.js

--- screen/Credeleck/index.js
+++ screen/Credeleck/index.js
@@ -1,15 +1,43 @@
-import { Dimensions } from "react-native"
+import { Dimensions, Image, ScrollView, TouchableOpacity } from "react-native"
 import { SafeAreaView } from "react-native"
 import { Text, View } from "react-native"
-import { PRIMARY } from "../../config/index";
-import { StatusBar } from "react-native";
+import { PRIMARY, SECUNDARY, SPACING } from "../../config/index";
+import { CredelecData } from "../../data/index";
+import { Ionicons } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
 const { width, height } = Dimensions.get('screen');
-export const Credelec = () => {
+export const Credelec = ({ navigation }) => {
     return (
         <SafeAreaView>
-            <Text>Make sure that 'Oneboard' is a valid screen name in your navigation setup. If the issue persists, you might want to provide more details about your navigation structure and how you're trying to navigate to the 'Oneboard' screen.
+            <StatusBar style="dark" />
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: SPACING * 5, marginLeft: SPACING, justifyContent: 'space-between' }}>
+                <TouchableOpacity>
+                    <Ionicons name="arrow-back" size={SPACING * 3} color={PRIMARY} />
+                </TouchableOpacity>
+                <Text style={{ flexDirection: 'row', fontWeight: 'bold', alignContent: 'center', alignSelf: 'center', alignItems: 'center', marginRight: SPACING * 3 }}>Credeleck</Text>
+                <Text></Text>
+            </View>
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
+                {CredelecData.map((category) =>
+                    <TouchableOpacity key={category.id} onPress={() => navigation.navigate('Credelec')}>
+                        <View style={{
+                            width: 125, height: 150, backgroundColor: PRIMARY,
+                            marginTop: SPACING * 2, borderRadius: SPACING, marginLeft: SPACING, padding: SPACING, justifyContent: 'space-between'
+                        }}>
+                            <Image source={category.img} style={{ width: 40, height: 40, justifyContent: 'space-between' }} />
+                            <Text style={{ color: SECUNDARY }}>{category.mame}</Text>
+                        </View>
+                    </TouchableOpacity>
+                )}
+            </ScrollView>
 
-            </Text>
+            <View style={{ justifyContent: 'center', alignContent: 'center', alignItems: 'center', padding: SPACING, marginTop: SPACING*5 }}>
+                <Text style={{ fontWeight: 'bold',fontSize:SPACING*10 }}>Favoritos</Text>
+                <Text style={{ backgroundColor: PRIMARY, width: 100, height: 4, borderRadius: SPACING }}></Text>
+            </View>
+            <View>
+                <Ionicons name="close" size={SPACING*5} color={PRIMARY}/>
+                <View/>
         </SafeAreaView>
     )
 }
