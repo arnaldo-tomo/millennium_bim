e56a7983de89f42a200627955bd0c81147e7579e
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-24 00:36:20 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,4 +4,4 @@ export const SPACING = 10;
 export const fundo = require('../assets/img/fundo.webp');
 export const banner = require('../assets/banner.jpg');
 
-export const user = { name: "ARNALDO", conta: 397153505, saldo: 168 }
+export const user = { name: "ARNALDO", conta: 397153505, saldo: 168 };
