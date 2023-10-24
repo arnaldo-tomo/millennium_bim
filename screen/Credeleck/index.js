aee4dd0130515291daf01ae2ef9a9de1e3fdf3d9
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-24 09:20:25 +0200

    Update index.js

--- screen/Credeleck/index.js
+++ screen/Credeleck/index.js
@@ -1,7 +1,7 @@
 import { Dimensions, Image, ScrollView, TouchableOpacity } from "react-native"
 import { SafeAreaView } from "react-native"
 import { Text, View } from "react-native"
-import { PRIMARY, SECUNDARY, SPACING } from "../../config/index";
+import { PRIMARY, Rosa, SECUNDARY, SPACING } from "../../config/index";
 import { CredelecData } from "../../data/index";
 import { Ionicons } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
@@ -14,30 +14,34 @@ export const Credelec = ({ navigation }) => {
                 <TouchableOpacity>
                     <Ionicons name="arrow-back" size={SPACING * 3} color={PRIMARY} />
                 </TouchableOpacity>
-                <Text style={{ flexDirection: 'row', fontWeight: 'bold', alignContent: 'center', alignSelf: 'center', alignItems: 'center', marginRight: SPACING * 3 }}>Credeleck</Text>
+                <Text style={{ flexDirection: 'row', fontWeight: 'bold', alignContent: 'center', alignSelf: 'center', alignItems: 'center', marginRight: SPACING * 5 }}>Credeleck</Text>
                 <Text></Text>
             </View>
             <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
                 {CredelecData.map((category) =>
-                    <TouchableOpacity key={category.id} onPress={() => navigation.navigate('Credelec')}>
+                    <TouchableOpacity key={category.id} onPress={() => navigation.push('CredeleckComprar')}>
                         <View style={{
                             width: 125, height: 150, backgroundColor: PRIMARY,
                             marginTop: SPACING * 2, borderRadius: SPACING, marginLeft: SPACING, padding: SPACING, justifyContent: 'space-between'
                         }}>
                             <Image source={category.img} style={{ width: 40, height: 40, justifyContent: 'space-between' }} />
-                            <Text style={{ color: SECUNDARY }}>{category.mame}</Text>
+                            <Text style={{ color: SECUNDARY }}>{category.name}</Text>
                         </View>
                     </TouchableOpacity>
                 )}
             </ScrollView>
 
-            <View style={{ justifyContent: 'center', alignContent: 'center', alignItems: 'center', padding: SPACING, marginTop: SPACING*5 }}>
-                <Text style={{ fontWeight: 'bold',fontSize:SPACING*10 }}>Favoritos</Text>
+            <View style={{ justifyContent: 'center', alignContent: 'center', alignItems: 'center', padding: SPACING, marginTop: SPACING * 5 }}>
+                <Text style={{ fontWeight: 'bold', fontSize: SPACING * 2 }}>Favoritos</Text>
                 <Text style={{ backgroundColor: PRIMARY, width: 100, height: 4, borderRadius: SPACING }}></Text>
             </View>
-            <View>
-                <Ionicons name="close" size={SPACING*5} color={PRIMARY}/>
-                <View/>
+
+            <View style={{ alignItems: 'center', justifyContent: 'center', alignSelf: 'center', alignItems: 'center', marginTop: SPACING * 10 }}>
+                <TouchableOpacity style={{ backgroundColor: "pink", borderRadius: SPACING * 5, padding: SPACING }}>
+                    <Ionicons name="close" size={SPACING * 4} color={PRIMARY} />
+                </TouchableOpacity>
+                <Text style={{ marginTop: SPACING }}>Nao tem favoritos guardados</Text>
+            </View>
         </SafeAreaView>
     )
 }
