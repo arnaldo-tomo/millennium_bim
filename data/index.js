c928441a3dc9e808512b25aaca20c365f74b913d
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-02 10:40:56 +0200

    Update index.js

--- data/index.js
+++ data/index.js
@@ -35,14 +35,38 @@ export const CredelecData =
         {
             id: 1,
             name: "Comprar",
-            img: require('../assets/img/icon/edmm.png'),
+            img: require('../assets/img/icon/shopping.png'),
         }, {
             id: 2,
             name: "Ultimas Recargas",
-            img: require('../assets/img/icon/edmm.png'),
+            img: require('../assets/img/icon/recargas.png'),
         }, {
             id: 3,
             name: "Infomacoes do contador",
-            img: require('../assets/img/icon/edmm.png'),
+            img: require('../assets/img/icon/contador.png'),
+        }
+    ]
+export const Recargas =
+    [
+        {
+            id: 1,
+            name: "Giro",
+            img: require('../assets/img/icon/tmcel.png'),
+        }, {
+            id: 2,
+            name: "Vodacom",
+            img: require('../assets/img/icon/vofafone.png'),
+        }, {
+            id: 3,
+            name: "Movitel",
+            img: require('../assets/img/icon/movitel.png'),
+        }, {
+            id: 4,
+            name: "Blà-Blà",
+            img: require('../assets/img/icon/tmcel.png'),
+        }, {
+            id: 5,
+            name: "Reenvio de Recargas",
+            img: require('../assets/img/icon/reload.png'),
         }
     ]
