d81cee3518de01dccf1817b4020a30e0b2f1508b
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-30 20:48:08 +0200

    Update stack.js

--- router/stack.js
+++ router/stack.js
@@ -13,7 +13,7 @@ export const StackNavigation = () => {
         <Stack.Navigator>
             <Stack.Screen name="dia" component={Dia} options={{ headerShown: false }} />
             <Stack.Screen name="Credelec" component={Credelec} options={{ headerShown: false }} />
-            <Stack.Screen name="CredeleckComprar" component={CredeleckComprar} />
+            <Stack.Screen name="CredeleckComprar" component={CredeleckComprar} options={{ headerShown:  false }} />
         </Stack.Navigator>
     );
 }
