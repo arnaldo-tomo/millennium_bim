b0f3ea0aef563e32045895a70f352dcd5141306b
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-30 09:40:15 +0200

    Update index.js

--- screen/transferir/index.js
+++ screen/transferir/index.js
@@ -2,20 +2,20 @@ import { Dimensions, Image, ScrollView, TouchableOpacity } from "react-native"
 import { SafeAreaView } from "react-native"
 import { Text, View } from "react-native"
 import { PRIMARY, Rosa, SECUNDARY, SPACING } from "../../config/index";
-import { CredelecData } from "../../data/index";
+import { CredelecData, Transferencias } from "../../data/index";
 import { Ionicons } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
 const { width, height } = Dimensions.get('screen');
 export const Transferir = ({ navigation }) => {
     return (
-        <SafeAreaView>
+        <SafeAreaView >
+
             <StatusBar style="dark" />
-            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: SPACING * 5, marginLeft: SPACING, justifyContent: 'space-between' }}>
-                <Text style={{ flexDirection: 'row', fontWeight: 'bold', alignContent: 'center', alignSelf: 'center', alignItems: 'center', marginRight: SPACING * 5 }}>Transferir</Text>
-                <Text></Text>
+            <View style={{ alignItems: 'center', marginTop: SPACING * 5, justifyContent: 'space-between' }}>
+                <Text style={{ fontWeight: 'bold', alignContent: 'center', alignSelf: 'center', alignItems: 'center', }}>Transferir</Text>
             </View>
             <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
-                {CredelecData.map((category) =>
+                {Transferencias.map((category) =>
                     <TouchableOpacity key={category.id} onPress={() => navigation.push('CredeleckComprar')}>
                         <View style={{
                             width: 125, height: 150, backgroundColor: PRIMARY,
@@ -32,13 +32,63 @@ export const Transferir = ({ navigation }) => {
                 <Text style={{ fontWeight: 'bold', fontSize: SPACING * 2 }}>Favoritos</Text>
                 <Text style={{ backgroundColor: PRIMARY, width: 100, height: 4, borderRadius: SPACING }}></Text>
             </View>
+            <ScrollView>
 
-            <View style={{ alignItems: 'center', justifyContent: 'center', alignSelf: 'center', alignItems: 'center', marginTop: SPACING * 10 }}>
-                <TouchableOpacity style={{ backgroundColor: "pink", borderRadius: SPACING * 5, padding: SPACING }}>
-                    <Ionicons name="close" size={SPACING * 4} color={PRIMARY} />
+                <TouchableOpacity style={{ backgroundColor: "white", borderRadius: 16, padding: SPACING, width: '90%', left: 20, height: 90, elevation: 1, marginBottom: 10 }}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center', alignItems: 'center', marginTop: 10 }}>
+                        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                            <Image source={require('../../assets/img/icon/vofafone.png')} style={{ backgroundColor: PRIMARY, borderRadius: 26, width: 50, height: 50, }} />
+                            <View style={{ marginLeft: 20, justifyContent: 'center' }}>
+                                <Text style={{ fontWeight: 'bold' }}>Mana Candida</Text>
+                                <Text style={{ fontSize: 15 }}>
+                                    <Text style={{ fontWeight: 'bold' }}>250.00</Text>
+                                    MZN
+                                </Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Ionicons name="chevron-forward" size={SPACING * 4} color={PRIMARY} />
+                        </View>
+                    </View>
                 </TouchableOpacity>
-                <Text style={{ marginTop: SPACING }}>Nao tem favoritos guardados</Text>
-            </View>
+
+                <TouchableOpacity style={{ backgroundColor: "white", borderRadius: 16, padding: SPACING, width: '90%', left: 20, height: 90, elevation: 1, marginBottom: 10 }}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center', alignItems: 'center', marginTop: 10 }}>
+                        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                            <Image source={require('../../assets/img/icon/vofafone.png')} style={{ backgroundColor: PRIMARY, borderRadius: 26, width: 50, height: 50, }} />
+                            <View style={{ marginLeft: 20, justifyContent: 'center' }}>
+                                <Text style={{ fontWeight: 'bold' }}>Mauro Peniel</Text>
+                                <Text style={{ fontSize: 15 }}>
+                                    <Text style={{ fontWeight: 'bold' }}>5,752.00</Text>
+                                    MZN
+                                </Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Ionicons name="chevron-forward" size={SPACING * 4} color={PRIMARY} />
+                        </View>
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={{ backgroundColor: "white", borderRadius: 16, padding: SPACING, width: '90%', left: 20, height: 90, elevation: 1, marginBottom: 10 }}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center', alignItems: 'center', marginTop: 10 }}>
+                        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                            <Image source={require('../../assets/img/icon/vofafone.png')} style={{ backgroundColor: PRIMARY, borderRadius: 26, width: 50, height: 50, }} />
+                            <View style={{ marginLeft: 20, justifyContent: 'center' }}>
+                                <Text style={{ fontWeight: 'bold' }}>Filho Mesada</Text>
+                                <Text style={{ fontSize: 15 }}>
+                                    <Text style={{ fontWeight: 'bold' }}>9,000.00</Text>
+                                    MZN
+                                </Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Ionicons name="chevron-forward" size={SPACING * 4} color={PRIMARY} />
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            </ScrollView>
+
         </SafeAreaView>
     )
 }
