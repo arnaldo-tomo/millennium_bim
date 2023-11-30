b35f508bc556e30b84da65b10d64a1f04793668e
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-30 20:47:44 +0200

    Update index.js

--- screen/dia-a-dia/index.js
+++ screen/dia-a-dia/index.js
@@ -3,7 +3,7 @@ import { SafeAreaView } from "react-native"
 import { Text, View } from "react-native"
 import { PRIMARY, SECUNDARY, SPACING, banner, fundo, user } from "../../config/index";
 import { ImageBackground } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import { StatusBar } from 'expo-status-bar';
 import { Ionicons } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native";
 import { useState } from "react";
@@ -16,65 +16,72 @@ export const Dia = ({ navigation }) => {
     const [active, setAtive] = useState(false);
 
     return (
-        <SafeAreaView>
-            <View style={{ width: width, height: height }}>
-                <StatusBar style="light" translucent animated />
-                <ImageBackground source={fundo} style={{ width: width, height: 340 }} >
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: SPACING, marginTop: SPACING * 3, alignItems: 'center' }}>
-                        <Text style={{ color: SECUNDARY, fontWeight: '500' }}>Ola, {user.name}</Text>
-                        <View style={{ justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center' }}>
-                            <TouchableOpacity>
-                                <Ionicons name="notifications" size={SPACING * 2} color={SECUNDARY} />
-                            </TouchableOpacity>
-                            <TouchableOpacity>
-                                <Ionicons name="settings" size={SPACING * 2} color={SECUNDARY} style={{ marginLeft: SPACING }} />
-                            </TouchableOpacity>
-                            <TouchableOpacity>
-                                <Ionicons name="power" size={SPACING * 2} color={SECUNDARY} style={{ marginLeft: SPACING }} />
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                    <View style={{ alignContent: 'center', alignSelf: 'center', alignItems: 'center' }}>
-                        <Text style={{ color: SECUNDARY, marginBottom: SPACING * 2 }}>CTA JOVEM MZN</Text>
-                        <Text style={{ color: SECUNDARY, marginBottom: SPACING * 2 }}>{user.conta}</Text>
+        <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+            <View style={{ width: width, height: height, backgroundColor: 'white' }}>
+                <StatusBar style="light" />
+                <View>
 
-                        {active == false ?
-                            <TouchableOpacity onPress={() => setAtive(true)}>
-                                <View style={{ flexDirection: 'row', alignItems: 'center', alignContent: 'center', alignSelf: 'center' }}>
-                                    <Ionicons name="eye" size={SPACING * 2} color={SECUNDARY} />
-                                    <Text style={{ color: SECUNDARY, fontSize: SPACING * 2, fontWeight: '800' }}> Ver saldo </Text>
+                    <ScrollView horizontal>
+
+                        <ImageBackground source={fundo} style={{ width: width, height: 340 }} >
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: SPACING, marginTop: SPACING * 3, alignItems: 'center' }}>
+                                <Text style={{ color: SECUNDARY, fontWeight: '500' }}>Ola, {user.name}</Text>
+                                <View style={{ justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center' }}>
+                                    <TouchableOpacity>
+                                        <Ionicons name="notifications" size={SPACING * 2} color={SECUNDARY} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity>
+                                        <Ionicons name="settings" size={SPACING * 2} color={SECUNDARY} style={{ marginLeft: SPACING }} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity>
+                                        <Ionicons name="power" size={SPACING * 2} color={SECUNDARY} style={{ marginLeft: SPACING }} />
+                                    </TouchableOpacity>
                                 </View>
+                            </View>
+                            <View style={{ alignContent: 'center', alignSelf: 'center', alignItems: 'center' }}>
+                                <Text style={{ color: SECUNDARY, marginBottom: SPACING * 2 }}>CTA JOVEM MZN</Text>
+                                <Text style={{ color: SECUNDARY, marginBottom: SPACING * 2 }}>{user.conta}</Text>
 
-                            </TouchableOpacity>
-                            :
-                            <TouchableOpacity onPress={() => setAtive(false)}>
-                                <View style={{ flexDirection: 'row', alignItems: 'center', alignContent: 'center', alignSelf: 'center' }}>
-                                    <Ionicons name="eye-off" size={SPACING * 2} color={'gray'} />
-                                    <Text style={{ color: SECUNDARY, fontSize: SPACING * 2, fontWeight: '800' }}>{user.saldo},200,42 </Text>
-                                    <Text style={{ color: SECUNDARY }}>MZN </Text>
+                                {active == false ?
+                                    <TouchableOpacity onPress={() => setAtive(true)}>
+                                        <View style={{ flexDirection: 'row', alignItems: 'center', alignContent: 'center', alignSelf: 'center' }}>
+                                            <Ionicons name="eye" size={SPACING * 2} color={SECUNDARY} />
+                                            <Text style={{ color: SECUNDARY, fontSize: SPACING * 2, fontWeight: '800' }}> Ver saldo </Text>
+                                        </View>
 
+                                    </TouchableOpacity>
+                                    :
+                                    <TouchableOpacity onPress={() => setAtive(false)}>
+                                        <View style={{ flexDirection: 'row', alignItems: 'center', alignContent: 'center', alignSelf: 'center' }}>
+                                            <Ionicons name="eye-off" size={SPACING * 2} color={'gray'} />
+                                            <Text style={{ color: SECUNDARY, fontSize: SPACING * 2, fontWeight: '800' }}>{user.saldo},200,42 </Text>
+                                            <Text style={{ color: SECUNDARY }}>MZN </Text>
+
+                                        </View>
+                                    </TouchableOpacity>
+                                }
+                            </View>
+                            <TouchableOpacity>
+
+                                <View style={{ position: 'absolute', justifyContent: 'flex-end', alignContent: 'flex-end', alignSelf: 'flex-end', alignItems: 'flex-end', right: 30, bottom: 0 }}>
+                                    <Ionicons name="chevron-forward" size={SPACING * 3} color={SECUNDARY} />
                                 </View>
                             </TouchableOpacity>
-                        }
-                    </View>
-                    <TouchableOpacity>
 
-                        <View style={{ position: 'absolute', justifyContent: 'flex-end', alignContent: 'flex-end', alignSelf: 'flex-end', alignItems: 'flex-end', right: 30, bottom: 0 }}>
-                            <Ionicons name="chevron-forward" size={SPACING * 3} color={SECUNDARY} />
-                        </View>
-                    </TouchableOpacity>
+                            <TouchableOpacity style={{
+                                borderColor: SECUNDARY, borderWidth: 1,
+                                borderRadius: SPACING - 4, alignItems: 'center', alignSelf: 'center', alignContent: 'center', padding: 5, marginTop: SPACING * 2, elevation: 10
+                            }}>
+                                <Text style={{ color: SECUNDARY, }}>Ver movimentos</Text>
+                            </TouchableOpacity>
+                            <View style={{ flexDirection: 'row', alignContent: 'center', alignItems: 'center', alignSelf: 'center', marginTop: SPACING + 2 }}>
+                                <Text style={{ backgroundColor: PRIMARY, width: 10, height: 10, borderRadius: SPACING }}></Text>
+                                <Text style={{ backgroundColor: SECUNDARY, width: 8, height: 8, borderRadius: SPACING, marginLeft: 3 }}></Text>
+                            </View>
+                        </ImageBackground>
 
-                    <TouchableOpacity style={{
-                        borderColor: SECUNDARY, borderWidth: 1,
-                        borderRadius: SPACING - 4, alignItems: 'center', alignSelf: 'center', alignContent: 'center', padding: 5, marginTop: SPACING * 2, elevation: 10
-                    }}>
-                        <Text style={{ color: SECUNDARY, }}>Ver movimentos</Text>
-                    </TouchableOpacity>
-                    <View style={{ flexDirection: 'row', alignContent: 'center', alignItems: 'center', alignSelf: 'center', marginTop: SPACING + 2 }}>
-                        <Text style={{ backgroundColor: PRIMARY, width: 10, height: 10, borderRadius: SPACING }}></Text>
-                        <Text style={{ backgroundColor: SECUNDARY, width: 8, height: 8, borderRadius: SPACING, marginLeft: 3 }}></Text>
-                    </View>
-                </ImageBackground>
+                    </ScrollView>
+                </View>
 
 
                 <ScrollView horizontal={true} style={{ position: 'absolute', top: 250 }} showsHorizontalScrollIndicator={false} >
@@ -90,9 +97,10 @@ export const Dia = ({ navigation }) => {
                         </TouchableOpacity>
                     )}
                 </ScrollView>
+
                 <View style={{ marginTop: SPACING * 10, justifyContent: 'center', alignItems: 'center', flexDirection: 'row', padding: SPACING }}>
                     <Ionicons name="pie-chart" color={'#258BE7'} size={20} />
-                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: "#258BE7" }}>Ver patrimonio</Text>
+                    <Text style={{ fontWeight: 'bold', color: "#258BE7" }}>Ver patrimonio</Text>
                 </View >
                 <Image source={banner} style={{ width: width, height: 120 }} resizeMode="cover" />
             </View>
